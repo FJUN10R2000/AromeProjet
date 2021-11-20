43b075e9f575e5c95dc0ed0f1c6561e0d0d742f4
FJUN10R2000 <fjunor-oliveira@bol.com.br>
2021-11-20 18:36:48 -0300

    Adicionar arquivos de projeto.

---
+++ ECommerce/Controllers/CompaniesController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ECommerce.Classes;
+using ECommerce.Models;
+
+namespace ECommerce.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        private EcommerceContext db = new EcommerceContext();
+
+        //controle de list view em cascata
+        public JsonResult GetCities (int departmentId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            var cities = db.Cities.Where(c => c.DepartamentsId == departmentId);
+            return Json(cities);
+
+        }
+
+
+        // GET: Companies
+        public ActionResult Index()
+        {
+            var companies = db.Companies.Include(c => c.Cities).Include(c => c.Departaments);
+            return View(companies.ToList());
+        }
+
+        // GET: Companies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            return View(company);
+        }
+
+        // GET: Companies/Create
+        public ActionResult Create()
+        {
+            ViewBag.CityId = new SelectList(CombosHelper.GetCities(), "CityId", "Name");
+            ViewBag.DepartamentsId = new SelectList(CombosHelper.GetDepartaments(), "DepartamentsId", "Name");
+            return View();
+        }
+
+        // POST: Companies/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CompanyId,Name,Phone,Address,Logo,DepartamentsId,CityId")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Companies.Add(company);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CityId = new SelectList(CombosHelper.GetCities(), "CityId", "Name", company.CityId);
+            ViewBag.DepartamentsId = new SelectList(CombosHelper.GetDepartaments(), "DepartamentsId", "Name", company.DepartamentsId);
+            return View(company);
+        }
+
+        // GET: Companies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CityId = new SelectList(CombosHelper.GetCities(), "CityId", "Name", company.CityId);
+            ViewBag.DepartamentsId = new SelectList(CombosHelper.GetDepartaments(), "DepartamentsId", "Name", company.DepartamentsId);
+            return View(company);
+        }
+
+        // POST: Companies/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CompanyId,Name,Phone,Address,Logo,DepartamentsId,CityId")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(company).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CityId = new SelectList(CombosHelper.GetCities(), "CityId", "Name", company.CityId);
+            ViewBag.DepartamentsId = new SelectList(CombosHelper.GetDepartaments(), "DepartamentsId", "Name", company.DepartamentsId);
+            return View(company);
+        }
+
+        // GET: Companies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            return View(company);
+        }
+
+        // POST: Companies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Company company = db.Companies.Find(id);
+            db.Companies.Remove(company);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ECommerce/Models/Company.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é requirido!!")]
+        [MaxLength(50, ErrorMessage = "O campo Nome recebe no máximo 50 caracteres")]
+        [Display(Name = "Nome")]
+        [Index("Departament_Name_Index", IsUnique = true)]
+
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo Telefone é requirido!!")]
+        [MaxLength(50, ErrorMessage = "O campo Telefone recebe no máximo 50 caracteres")]
+        [Display(Name = "Telefone")]
+        //[Index("Departament_Name_Index", IsUnique = true)]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "O campo Endereço é requirido!!")]
+        [MaxLength(50, ErrorMessage = "O campo Endereço recebe no máximo 50 caracteres")]
+        [Display(Name = "Endereço")]
+        public string Address { get; set; }
+
+        [Display(Name = "Imagem")]
+        [DataType(DataType.ImageUrl)]
+        public string Logo { get; set; }
+
+        [Required(ErrorMessage = "O campo Departamento é requirido!!")]
+        [Display(Name = "Departamento")]
+        public int DepartamentsId { get; set; }
+
+        [Required(ErrorMessage = "O campo Cidade é requirido!!")]
+        [Display(Name = "Cidade")]
+        public int CityId { get; set; }
+
+        public virtual Departaments Departaments { get; set; }
+        public virtual City Cities { get; set; }
+    }
+}
